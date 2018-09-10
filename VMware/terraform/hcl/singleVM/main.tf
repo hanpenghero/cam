f443341a3dfb0592093b1df02a04fc19dd2c0c2e
hanpenghero <hanpenghero@hotmail.com>
2018-09-10 18:24:19 +0800

    Add files via upload

--- VMware/terraform/hcl/singleVM/main.tf
+++ VMware/terraform/hcl/singleVM/main.tf
@@ -207,6 +207,8 @@ resource "vsphere_virtual_machine" "vm_1" {
 resource "null_resource" "cluster" {
 
     provisioner "local-exec" {
-            command = "echo ${var.DB_Server_IP} >> private_ips.txt"
+	inline = [
+	    "echo ${var.DB_Server_IP} >> private_ips.txt"
+	]
     }
 }
